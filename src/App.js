daf05996c50238cd3d49149f3cbece7d0e9199b1
Olli Savisaari <olli@Ollis-MBP.localdomain>
2019-02-17 17:00:23 +0200

    muutokset

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import React from "react";
+import "./App.css";
+import "././assets/ylös.svg";
+import "././assets/oikea.svg";
+import "././assets/vasen.svg";
+
+var arrows = ["1", "2", "3"];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  renderSquare() {
+    return <Square 
+    value={this.state} 
+    onClick={() => this.handleClick()} />;
+  }
+  handleClick(i) {
+    const Square = this.state.();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+  }
   render() {
+    let status;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header" />
+        <body>
+          <div class="square-container" width="100%" height="100%">
+            <svg class="square" />
+            {/*}  <div className="image" /> */}
+          </div>
+        </body>
       </div>
     );
   }
 }
 
+console.log(arrows[1]);
 export default App;
