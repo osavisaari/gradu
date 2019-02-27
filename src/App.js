e525144ae04c5538dc2c3afbfbffd0c8ccc31a8e
Olli Savisaari <olli@Ollis-MBP.localdomain>
2019-02-27 14:32:09 +0200

    felixin btnListenerit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,76 @@
 import React, { Component } from "react";
 import "./App.css";
-/*import "./assets/ylös.svg";
-import "./assets/oikea.svg";
-import "./assets/vasen.svg";
-miten mä tuon fiksusti kaikki tarvittavat ärsykekuvat tähän?
+import A from "./assets/A.png";
+import oikea from "./assets/oikea.svg";
+import vasen from "./assets/vasen.svg";
+import ylös from "./assets/ylös.svg";
+import alas from "./assets/alas.svg";
+/*miten mä tuon fiksusti kaikki tarvittavat ärsykekuvat tähän?
 Tarttis helpon tavan tuoda kansion sisältö yhteen arrayyn */
 
+const suunnat = [oikea, vasen, ylös, alas];
+const ESCAPE_KEY = 27;
+const UP = 38;
+const DOWN = 40;
+const LEFT = 37;
+const RIGHT = 39;
+
 class App extends Component {
-  state = { label: this.state };
+  state = {
+    suunta: oikea
+  };
+
+  handleKeyDown = event => {
+    switch (event.keyCode) {
+      case ESCAPE_KEY:
+        this.addArrow();
+        break;
+      case UP:
+        this.handleUp();
+        break;
+      case DOWN:
+        this.handleDown();
+        break;
+      case LEFT:
+        this.handleLeft();
+        break;
+      case RIGHT:
+        this.handleRight();
+        break;
+    }
+  };
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  handleUp = () => {
+    if (this.state.suunta == ylös) {
+      this.setState({ suunta: null });
+    }
+  };
 
   handleClick = () => {
-    if (this.state.label === this.setState) {
-      this.setState({
-        state: (this.setState.label = "false")
-      });
-    } else {
-      this.setState({
-        state: (this.setState.label = "2")
-      });
-    }
+    this.setState({ suunta: null });
+  };
+
+  addArrow = () => {
+    const randomIndex = Math.floor(Math.random() * 4);
+    const nuoli = suunnat[randomIndex];
+    this.setState({ suunta: nuoli }); // valitse randomilla
   };
 
   render() {
     return (
       <div>
-        <img src="A.png" width="50px" height="50px" alt="Aa" />
-        <div className="square" onClick={this.handleClick} />
+        <img src={A} alt="Aa" />
+        <div className="square" onClick={this.handleClick}>
+          {this.state.suunta && <img src={this.state.suunta} alt="arrow" />}
+        </div>
       </div>
     );
   }
