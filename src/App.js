d689bed97cd7c030be33709d27a09271da2b6757
Olli Savisaari <olli@Ollis-MBP.localdomain>
2019-02-28 15:17:26 +0200

    flex-muutokset

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-import A from "./assets/A.png";
+import A from "./assets/A.svg";
+import B from "./assets/B.svg";
+import C from "./assets/C.svg";
+import D from "./assets/D.svg";
+import E from "./assets/E.svg";
 import oikea from "./assets/oikea.svg";
 import vasen from "./assets/vasen.svg";
 import ylös from "./assets/ylös.svg";
@@ -49,7 +53,7 @@ class App extends Component {
   }
 
   handleUp = () => {
-    if (this.state.suunta == ylös) {
+    if (this.state.suunta === ylös) {
       this.setState({ suunta: null });
     }
   };
@@ -66,10 +70,18 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <img src={A} alt="Aa" />
-        <div className="square" onClick={this.handleClick}>
-          {this.state.suunta && <img src={this.state.suunta} alt="arrow" />}
+      <div className="container">
+        <div className="cards">
+          <img src={A} className="card" alt="A" />
+          <img src={B} className="card" alt="B" />
+          <img src={C} className="card" alt="C" />
+          <img src={D} className="card" alt="D" />
+          <img src={E} className="card" alt="E" />
+        </div>
+        <div className="square-container">
+          <div className="square" onClick={this.handleClick}>
+            {this.state.suunta && <img src={this.state.suunta} alt="arrow" />}
+          </div>
         </div>
       </div>
     );
